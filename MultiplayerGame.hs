b2477c4f3231699a8ff7f123538e4c9e0864c372
Karinna Loo <kloo@seas.upenn.edu>
2017-04-28 03:58:59 -0400

    final changes

--- MultiplayerGame.hs
+++ MultiplayerGame.hs
@@ -92,3 +92,39 @@ playTurn handle chan isServer deck board src input = withSocketsDo $
               displayBoard board'
               mainLoop handle chan isServer deck' board'
           updateGameState = mainLoop handle chan isServer deck
+
+------------------- OLD VERSION OF THIS LOOP FOR DEMO ------------------------
+-- +createGame :: Handle -> Chan (Int, String) -> Bool -> IO ()
+-- +createGame handle chan isServer = withSocketsDo $ do
+-- +    putStrLn "Created a new game."
+-- +    when isServer (hPutStrLn handle (show generateBoard))
+-- +    mainLoop handle chan isServer
+-- +
+-- +mainLoop :: Handle -> Chan (Int, String) -> Bool -> IO ()
+-- +mainLoop handle chan isServer = withSocketsDo $ do
+-- +    (src, input) <- readChan chan
+-- +    if input == "exit"
+-- +      then return ()
+-- +      else do
+-- +        if src == 0 -- 0 means stdin, 1 means a network message (fix this later)
+-- +        -- CASE: USER INPUT
+-- +        then do
+-- +          hPutStr handle (if isServer then "Server: " else "Client: ")
+-- +          hPutStrLn handle input
+-- +          -- Replace above with code that validates whether the user's input was a valid set, and if so sends a network msg
+-- +          -- Here is some very basic sample code:
+-- +          -- if (validateSet (parseSet input))
+-- +          --   then do
+-- +          --     hPutStrLn handle (input)
+-- +          --     when isServer (hPutStrLn handle (show generateBoard))
+-- +          --   else do
+-- +          --     putStrLn "Not a valid set!"
+-- +        -- CASE: NETWORK MSG
+-- +        else do
+-- +          putStrLn input
+-- +          -- Replace above with code that prints the set that the other player found
+-- +          -- If this player is a client, read the next message atomically and replace the game board
+-- +          -- Basic sample code, not including regenerating the board:
+-- +          -- putStr "Other player found the set: "
+-- +          -- putStrLn input
+-- +        mainLoop handle chan isServer
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # CIS 552 Final Project
 ### Karinna Loo and Carolina Zheng
+
+Carolina Zheng (carzheng)
+Karinna Loo (kloo)
+For our final project we built a networked version of the game Set. We completed single player and two-player versions of the game. The main components of our project include:
+
+Logic.hs: contains all the game logic and parsing functions
+Main.hs: that sets up the networking (for two-player only) and concurrency
+MultiplayerGame.hs/SinglePlayerGame.hs: contains the main game loops
+Test modules:
+Tests.hs: contains unit and QuickCheck tests for individual game logic functions
+TestSinglePlayer.hs: simply runs a single player game to completion
+TestClient.hs: is a client that will connect to a server and automatically play the game, printing the game state to the console as well as running validation tests after every turn.
+
+We didn’t use any external libraries, but we did use the Parser.hs and ParserCombinators.hs files from class.
